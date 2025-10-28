1c9caee94f8ec7908524dbc66b4815a55dacc54e
Raphael <107597299+raphaelsette@users.noreply.github.com>
2025-10-28 18:21:30 +0000

    add array numbers and speechtext

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial() {
@@ -24,7 +27,7 @@ function verificarChute() {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', `O número secreto é menor que ${chute}!`);
         } else {
-            exibirTextoNaTela('p', `O número secreto é maior ${chute}!`);
+            exibirTextoNaTela('p', `O número secreto é maior que ${chute}!`);
         }
         tentativas++;
         limparCampo();
@@ -32,7 +35,19 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo(){
